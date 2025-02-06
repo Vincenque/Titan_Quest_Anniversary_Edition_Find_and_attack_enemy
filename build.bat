388116f21c2c8a14fb63f3e70aa0f3dd769c867a
Vincenque <DudaWitold.DW@gmail.com>
2025-02-06 15:41:29 +0100

    Changed 'program.exe' to 'Titan Quest Anniversary Edition - Find and attack enemy.exe'

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 
 rem Remove the old executable if it exists
-if exist "%~dp0bin\program.exe" del /q "%~dp0bin\program.exe"
+if exist "%~dp0bin\program.exe" del /q "%~dp0bin\Titan Quest Anniversary Edition - Find and attack enemy.exe.exe"
 
 rem Create the bin directory if it doesn't exist
 if not exist "%~dp0bin" mkdir "%~dp0bin"
 
 rem Compile with debugging symbols (-g flag)
-gcc -g -I"%~dp0headers" -LC:/MinGW/lib "%~dp0source\main.c" -o "%~dp0bin\program.exe" -lmingw32
+gcc -g -I"%~dp0headers" -LC:/MinGW/lib "%~dp0source\main.c" -o "%~dp0bin\Titan Quest Anniversary Edition - Find and attack enemy.exe" -lmingw32
